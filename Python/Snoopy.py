035af51c4d405ce3bcfdae08cea41aabbb547622
Balti <balti@ejemplo.com>
2025-12-26 20:26:29 -0300

    Balti empieza su camino de developer

---
+++ Python/Snoopy.py
@@ -0,0 +1,8 @@
+print("hi")
+input("credit card number")
+input ("debit card number")
+input("bank credit card")
+input("bank debit card")
+input("real name")
+input("do you have a pet ,yes  is 70 d no is 1000000 d ")
+print("thanks for catch your money")
